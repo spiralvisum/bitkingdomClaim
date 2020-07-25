b8da316aae5c2b8a96f16d9481a5bd1eb8e5210b
Nathan Horton <nathanh@audiobooksnow.com>
2020-07-25 12:44:03 -0500

    removed unused shit

--- pages/index.vue
+++ pages/index.vue
@@ -106,6 +106,9 @@
           phoneValid:(phone) => { //a custom validator!
       	    return /^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$/.test(phone)
           }
+        },
+        amount: {
+          required
         }
       }
     },
--- plugins/validators.js
+++
@@ -1,4 +0,0 @@
-export default function validatePhone (value) {
-    let mobileRegx = /^\+94[0-9]{9}$/;
-    return mobileRegx.test(value);
-}
